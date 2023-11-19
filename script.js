6f26d8bf5ce56118673fa0c701ae829d3646bc09
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-19 09:35:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
-}
+	if(s1.length === 0 && s2.length === 0){
+		return "";
+	}
+	let first = s1.charAt(0);
+	let second = s2.charAt(0);
+	return first + second;
+} 
 
-// Do not change the code below.
+// Do not change the code below. 
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
